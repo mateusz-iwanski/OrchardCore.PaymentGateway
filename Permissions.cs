9e4c8157ed0f3a2d8d0ada1541dc9efb196a84a4
Mateusz Mateusz <mateusz.iw@gmail.com>
2026-01-20 18:38:14 +0100

    Add ManagePaymentGateway permission for Przelewy24
    Introduced a Permissions class implementing IPermissionProvider for the OrchardCore module. Added a ManagePaymentGateway permission to control access to Przelewy24 payment gateway settings, granting it by default to Administrator and SiteOwner roles.

---
+++ Permissions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using OrchardCore.Security.Permissions;
+
+namespace OrchardCore.PaymentGateway;
+
+public class Permissions : IPermissionProvider
+{
+    public static readonly Permission ManagePaymentGateway =
+        new Permission(nameof(ManagePaymentGateway), "Manage Przelewy24 payment gateway settings");
+
+    public Task<IEnumerable<Permission>> GetPermissionsAsync() =>
+        Task.FromResult<IEnumerable<Permission>>(new[] { ManagePaymentGateway });
+
+    public IEnumerable<Permission> GetPermissions() =>
+        new[] { ManagePaymentGateway };
+
+    public IEnumerable<PermissionStereotype> GetDefaultStereotypes() =>
+        new[]
+        {
+            new PermissionStereotype
+            {
+                Name = "Administrator",
+                Permissions = new[] { ManagePaymentGateway }
+            },
+            new PermissionStereotype
+            {
+                Name = "SiteOwner",
+                Permissions = new[] { ManagePaymentGateway }
+            }
+        };
+}
