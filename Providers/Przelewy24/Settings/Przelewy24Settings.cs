166694ff03f2ffee7a147def62c40808e9409bb3
Mateusz Mateusz <mateusz.iw@gmail.com>
2026-01-20 18:37:00 +0100

    Add Przelewy24Settings class for gateway configuration
    Introduced Przelewy24Settings class to encapsulate configuration
    options for the Przelewy24 payment gateway, including credentials,
    keys, base URL, and a sandbox fallback flag. This will facilitate
    integration and management of Przelewy24 settings.

---
+++ Providers/Przelewy24/Settings/Przelewy24Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrchardCore.PaymentGateway.Providers.Przelewy24.Settings;
+
+public class Przelewy24Settings
+{
+    public string? ClientId { get; set; }
+    public int? MerchantId { get; set; }
+    public int? PosId { get; set; }
+    public string? CrcKey { get; set; }
+    public string? ReportKey { get; set; }
+    public string? SecretId { get; set; }
+    public string? BaseUrl { get; set; }
+    public bool UseSandboxFallbacks { get; set; } = true;
+}
